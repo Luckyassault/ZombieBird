8cbad9a74b983201fc5b93a693a36c2acf937731
Rene Tang <renetang00@gmail.com>
2019-01-01 11:50:19 +0800

    Day 4: Create and renders a rectangle that moves horizontally

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -1,6 +1,7 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
@@ -24,5 +25,29 @@ public class GameRenderer {
 
     public void render(){
         Gdx.app.log("GameRenderer","render");
+
+
+         //1. We draw a black background. This prevents flickering.
+
+          Gdx.gl.glClearColor(0,0,0,1);
+          Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+          //2. Draw a filled rectangle
+        // Tells shapeRenderer to begin drawing filled shapes
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+        shapeRenderer.rect(myWorld.getRect().x,myWorld.getRect().y,myWorld.getRect().width,myWorld.getRect().height);
+
+        shapeRenderer.end();
+
+        //3. Draw Rectangle's outline
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+        shapeRenderer.rect(myWorld.getRect().x,myWorld.getRect().y,myWorld.getRect().width,myWorld.getRect().height);
+
+        shapeRenderer.end();
+
     }
 }
