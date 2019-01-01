3fe4fd3d3e6bd0abb1df3eead03cd752521a2af5
Rene Tang <renetang00@gmail.com>
2019-01-01 11:37:15 +0800

    Day 4: Create shapeRender method

--- core/src/com/kilobolt/gameworld/GameRenderer.java
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -1,6 +1,8 @@
 package com.kilobolt.gameworld;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 /**
  * Created by Rene Ambrose Tang on 1/1/2019.
@@ -9,9 +11,15 @@ import com.badlogic.gdx.Gdx;
 public class GameRenderer {
 
     private GameWorld myWorld;
+    private OrthographicCamera cam;
+    private ShapeRenderer shapeRenderer;
 
     public GameRenderer(GameWorld world){
         myWorld = world;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true,136,204);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setProjectionMatrix(cam.combined);
     }
 
     public void render(){
